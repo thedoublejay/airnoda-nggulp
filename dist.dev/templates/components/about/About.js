0339ba8d3f0591e3f5352c36230f20f6680f38c4
JJ Adonis <siikalawangjay@gmail.com>
2017-03-29 01:03:48 +0800

    Header Responsive
    Finished the development for responsiveness of header

--- dist.dev/templates/components/about/About.js
+++ dist.dev/templates/components/about/About.js
@@ -6,6 +6,6 @@ try {
 }
 module.run(['$templateCache', function($templateCache) {
   $templateCache.put('components/about/About.html',
-    '<div class="container-fluid"><div class="arnd-body-message"><i class="fa fa-wrench" aria-hidden="true"></i><h1>Site still in progress.</h1></div></div>');
+    '<div class="container-fluid"><div class="arnd-body-message"><i class="fa fa-5x fa-wrench" aria-hidden="true"></i><h1>Site still in progress.</h1></div></div>');
 }]);
 })();
