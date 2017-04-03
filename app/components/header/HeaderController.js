535388b434ba93302f1e7c177105615da55fbe55
JJ Adonis <siikalawangjay@gmail.com>
2017-04-03 12:53:30 +0800

    Contact Section codes

--- app/components/header/HeaderController.js
+++ app/components/header/HeaderController.js
@@ -3,17 +3,19 @@
     var app = angular.module("airnodaApp");
 
 
-    var HeaderController = function ($scope, $location) {
+    var HeaderController = function ($scope, $location, $anchorScroll) {
         var HeaderCtrl = this;
         
         HeaderCtrl.currentPage = $location.path(); 
         HeaderCtrl.changePage = function(moveTo){
             $location.path(moveTo);
             HeaderCtrl.currentPage = '/' + moveTo;
+            $location.hash('view-id');
+            $anchorScroll();
         };
     };
     
-    HeaderController.$inject = [ '$scope', '$location' ];
+    HeaderController.$inject = [ '$scope', '$location', '$anchorScroll' ];
     app.controller("HeaderController", HeaderController);
 
 }());
